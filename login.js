378ef333e21d88b96b9624d80d3f63041312c0a4
HafiQod <23523084@students.uii.ac.id>
2024-12-21 14:40:32 +0700

    backgrnd indx

--- login.js
+++ login.js
@@ -1,11 +1,14 @@
 const registerButton = document.getElementById("register");
 const loginButton = document.getElementById("login");
 const container = document.getElementById("container");
+const body = document.body;
 
 registerButton.addEventListener("click", () => {
-    container.classList.add("right-panel-active")
-})
+    container.classList.add("right-panel-active");
+    body.classList.add("work-it-bg");
+});
 
 loginButton.addEventListener("click", () => {
-    container.classList.remove("right-panel-active")
-})
+    container.classList.remove("right-panel-active");
+    body.classList.remove("work-it-bg");
+});
